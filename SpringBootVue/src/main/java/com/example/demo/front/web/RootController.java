34b4b17d8768151d2d41b23006c768d2df000fc4
jsj72 <jsj72@DESKTOP-TH9MNQ9>
2019-05-02 22:17:50 +0900

    package 명 변경

--- SpringBootVue/src/main/java/com/example/demo/front/web/RootController.java
+++ SpringBootVue/src/main/java/com/example/demo/front/web/RootController.java
@@ -1,4 +1,4 @@
-package com.example.demo.front.controller;
+package com.example.demo.front.web;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
